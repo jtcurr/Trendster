e156071d65cee7ebb2b225b9296f7ac62670e5db
Daniel Voigt <danielralphvoigt@gmail.com>
2017-02-04 14:06:56 -0800

    style update

--- src/client/app/ListEntry.jsx
+++ src/client/app/ListEntry.jsx
@@ -4,16 +4,14 @@ var ListEntry = (props, key, {}) => {
   var prefix = props.venueName.venue.photos.groups[0].items[0].prefix;
   var suffix = props.venueName.venue.photos.groups[0].items[0].suffix;
 	return (
-		<li className='list-item'>
+		<div className='list-item'>
       <a href={props.venueName.venue.url} className='list-link'>{props.venueName.venue.name}</a>
-      <ul>
-        <li>
-          {props.venueName.venue.location.address}
-          <br></br>
-          <img src={ prefix + '100x100' + suffix } className='list-image'></img> 
-        </li>
-      </ul>
-    </li>
+      <div>
+        {props.venueName.venue.location.address}
+        <br></br>
+        <img src={ prefix + '100x100' + suffix } className='list-image'></img> 
+      </div>
+    </div>
 	)
 }
 
